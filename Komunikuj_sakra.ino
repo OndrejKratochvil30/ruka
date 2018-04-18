6be2fa03fbe18b532176f7e22fa0b3bdc445460b
OndrejKratochvil30 <38500053+OndrejKratochvil30@users.noreply.github.com>
2018-04-18 16:03:07 +0200

    Add files via upload

---
+++ Komunikuj_sakra.ino
@@ -0,0 +1,28 @@
+#include <LearningKit.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(115200);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ /* if (connected != Serial2.hasClient()) {
+    if (SerialBT.hasClient() == true) {
+      connected = true;
+      lcd.setCursor(0, 3);
+      lcd.print("                    ");
+      lcd.print("Conected     ");
+    } else {
+      connected = false;
+      lcd.setCursor(0, 3);
+      lcd.print("                    ");
+      lcd.print("Disconected");
+    }*/
+  }
+  if (Serial.available()) {
+    Serial.write(Serial1.read());
+
+  }
